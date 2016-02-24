40102f59ac0ab74476ee507c77f7d4c203a90bde
CarrieCramer <carrie15@tampabay.rr.com>
2016-02-24 08:59:40 -0500

    Added Pricing Policy 8 header with updatePrices8() prototype

---
+++ PricingPolicy8.h
@@ -0,0 +1,10 @@
+#ifndef PRICING_POLICY_8
+#define PRICING_POLICY_8
+
+// Pricing policy 8: System Optimal Pricing - Bilevel Pricing Game
+// **************************************************************ADD DESCRIPTION****************************************************
+
+void updatePrices8();
+
+
+#endif
